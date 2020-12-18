b93770562dba0467967adca0163c2a6d0a2a609d
SkynetDev <andrejohnson0017@gmail.com>
2020-12-18 14:25:01 -0500

    added string intrinsics 2

--- variable.sol
+++ variable.sol
@@ -14,7 +14,8 @@ contract WorkingwithVariables {
     }
     
     uint8 public myUint8;
-    
+    // integers silently wrap around
+
     function incrementUint () public {
         myUint8++;
     }
@@ -28,12 +29,12 @@ contract WorkingwithVariables {
     function setMyAddress (address _myAddress) public {
         myAddress = _myAddress;
     }
-    
+    // The "address" type has atleast one method "balance"
     function getBalanceofAddress() public view returns(uint) {
         return myAddress.balance;
     }
     string public myString = "Hello world";
-    
+    // strings and other reference types are stored in memory
     function setMyString (string memory _myString) public {
         myString = _myString;
     }
